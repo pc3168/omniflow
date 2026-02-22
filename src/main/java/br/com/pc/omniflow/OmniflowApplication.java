fa1401dc855ddb66f3c4480551b2296158e3284c
paulo <pc3168@gmail.com>
2026-02-21 22:28:42 -0300

    Iniciando o projeto omniflow

---
+++ src/main/java/br/com/pc/omniflow/OmniflowApplication.java
@@ -0,0 +1,13 @@
+package br.com.pc.omniflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmniflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmniflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pc/omniflow/OmniflowApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pc.omniflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmniflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
